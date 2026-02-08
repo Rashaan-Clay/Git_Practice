0c85bc6109eaaf95559141167ebe0abc2a4c9049
Rashaan Clay <rashaanrhclay@gmail.com>
2026-02-08 16:15:24 -0500

    Completed L07 Assignment

---
+++ L07.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+/*
+    Prompts the user to enter text
+    You must display a message with instructions
+    Use the fgets function to read-in the user input
+    Converts the input entirely to uppercase letters
+    Prints the resulting string
+*/
+
+int main(void)
+{
+    char input[100];
+    fgets(input, 100, stdin);
+    size_t n = strnlen(input, 100);
+
+    for (int i = 0; i < n; i++)
+    {
+        input[i] = toupper(input[i]);
+    }
+
+    printf("%s", input);
+
+    return 0;
+}
--- lecture_tests.c
+++ lecture_tests.c
@@ -2,7 +2,8 @@
 
 int main(void)
 {
-    printf("Hello World");
-    
-    return 0;
+    int arr[] = {1,2,3,4,5,51,12,123,123,1321,3123,123,112312,};
+
+    printf("%d", sizeof(arr) / sizeof(arr[0]));
+
 }
